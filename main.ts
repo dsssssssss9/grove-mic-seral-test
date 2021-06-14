2956bc668522bdc6369f240ead66b8a7a051b6cf
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-06-14 20:56:57 +0100

    define variable for no sound and pause display

--- main.ts
+++ main.ts
@@ -1,10 +1,12 @@
+let No_Sound = 16
 let colorbit_51bit = colorbit.initColorBit(DigitalPin.P2, BitColorMode.RGB)
 colorbit_51bit.clear()
 colorbit_51bit.showColorIcon(ColorIcon.ChristmasTree, colorbit.colors(BitColors.Red))
 basic.pause(200)
 basic.forever(function () {
-    if (pins.analogReadPin(AnalogPin.P1) >= 13) {
-        colorbit_51bit.showBarGraph(pins.analogReadPin(AnalogPin.P1), 1023)
+    if (pins.analogReadPin(AnalogPin.P1) >= No_Sound) {
+        colorbit_51bit.showBarGraph(pins.analogReadPin(AnalogPin.P1), 255)
+        basic.pause(500)
     } else {
         colorbit_51bit.clear()
     }
