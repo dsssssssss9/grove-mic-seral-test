87b6e1201c93b3fa363044372237f53c4772a8bf
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-06-14 20:48:59 +0100

    clear colorbit is level less than silence approx 12

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
 let colorbit_51bit = colorbit.initColorBit(DigitalPin.P2, BitColorMode.RGB)
 colorbit_51bit.clear()
 colorbit_51bit.showColorIcon(ColorIcon.ChristmasTree, colorbit.colors(BitColors.Red))
-basic.pause(1000)
+basic.pause(200)
 basic.forever(function () {
-    while (pins.analogReadPin(AnalogPin.P1) >= 13) {
-        colorbit_51bit.showBarGraph(pins.analogReadPin(AnalogPin.P1), 255)
+    if (pins.analogReadPin(AnalogPin.P1) >= 13) {
+        colorbit_51bit.showBarGraph(pins.analogReadPin(AnalogPin.P1), 1023)
+    } else {
+        colorbit_51bit.clear()
     }
 })
