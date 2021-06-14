7766839334038518e2c641d1c40ba1e8e50c27ba
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-06-14 20:45:55 +0100

    install colorbit extension and do basic graph of sound level

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+let colorbit_51bit = colorbit.initColorBit(DigitalPin.P2, BitColorMode.RGB)
+colorbit_51bit.clear()
+colorbit_51bit.showColorIcon(ColorIcon.ChristmasTree, colorbit.colors(BitColors.Red))
+basic.pause(1000)
 basic.forever(function () {
-    serial.writeValue("Vol", pins.analogReadPin(AnalogPin.P1))
+    while (pins.analogReadPin(AnalogPin.P1) >= 13) {
+        colorbit_51bit.showBarGraph(pins.analogReadPin(AnalogPin.P1), 255)
+    }
 })
